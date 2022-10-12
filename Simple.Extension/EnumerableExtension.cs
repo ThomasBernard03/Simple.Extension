5402ac70165cbc8c7aaa7b08afc0e496ed80136f
Thomas Bernard <thomas.bernard@diiage.org>
2022-10-12 21:03:15 +0200

    Enumerable extension

---
+++ Simple.Extension/EnumerableExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Simple.Extension.Enumerable;
+
+public static class EnumerableExtension
+{
+    public static bool IsNullOrEmpty<T>(this IEnumerable<T> value)
+    {
+        return value?.Any() ?? false;
+    }
+
+    public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> value)
+    {
+        return !(value?.Any() ?? false);
+    }
+}
+
